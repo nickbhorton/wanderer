e010398908821de4bc05b7a5c63a3c44d6ae610e
Nick Horton <nickkbhorton@gmail.com>
2024-06-24 09:25:44 -0500

    starting uploading process

--- website/deploy.bash
+++ website/deploy.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
-killall meadow
 fuser 8080/tcp
 meadow &
 MEADOW_PID=$!
-echo $MEADOW_PID
+trap "kill $MEADOW_PID" SIGINT
+echo "meadow pid: $MEADOW_PID"
 echo "Compile tailwindcss"
 npx tailwindcss -i src/styles.css -o build/output.css --watch
-kill $MEADOW_PID
